b42981b196f4e9759f3200007c84a778a883f750
John Bartos <bartsjb@twitch.tv>
2021-02-09 16:55:17 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -71,9 +71,8 @@ module.exports = {
                  *
                  * 'amazon-ivs-player/dist/assets/amazon-ivs-wasmworker.min.wasm'
                  * 'amazon-ivs-player/dist/assets/amazon-ivs-wasmworker.min.js';
-                 * 'amazon-ivs-player/dist/assets/amazon-ivs-worker.min.js';
                  *
-                 * These assets must not be re-compiled during packaging.
+                 * These assets must not be re-compiled during packaging. Your build tool must ignore these wasm files.
                  * The webpack file-loader (https://webpack.js.org/loaders/file-loader/) accomplishes this.
                  * Rollup's plugin-url (https://github.com/rollup/plugins/tree/master/packages/url) also seems to do this, but has not been tested.
                  */
