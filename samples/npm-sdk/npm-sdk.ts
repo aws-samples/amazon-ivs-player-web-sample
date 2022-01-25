ddcf5dbd3523d10964feac6ef9d7609fe0bd1082
John Bartos <bartsjb@twitch.tv>
2022-01-25 15:16:29 -0500

    Update to webpack 5 to resolve dep security issues

--- samples/npm-sdk/npm-sdk.ts
+++ samples/npm-sdk/npm-sdk.ts
@@ -1,14 +1,14 @@
 import {
-    create,
-    ErrorType,
-    isPlayerSupported,
-    MediaPlayer,
-    PlayerError,
-    PlayerEventType,
-    PlayerState,
-    Quality,
-    TextCue,
-    TextMetadataCue,
+  create,
+  ErrorType,
+  isPlayerSupported,
+  MediaPlayer,
+  PlayerError,
+  PlayerEventType,
+  PlayerState,
+  Quality,
+  TextCue,
+  TextMetadataCue,
 } from 'amazon-ivs-player';
 import { setupForm, getFormStream } from '../common/form-control';
 
@@ -17,95 +17,106 @@ import { setupForm, getFormStream } from '../common/form-control';
  * We use the TypeScript compiler (TSC) to check types; it doesn't know what this WASM module is, so let's ignore the error it throws (TS2307).
  */
 // @ts-ignore
-import wasmBinaryPath from 'amazon-ivs-player/dist/assets/amazon-ivs-wasmworker.min.wasm'
+import wasmBinaryPath from 'amazon-ivs-player/dist/assets/amazon-ivs-wasmworker.min.wasm';
 import wasmWorkerPath from 'amazon-ivs-player/dist/assets/amazon-ivs-wasmworker.min.js';
 
 class PlayerDemo {
-    private player: MediaPlayer;
-    private videoElement: HTMLVideoElement = document.querySelector('#video-player');
+  private player: MediaPlayer;
+  private videoElement: HTMLVideoElement =
+    document.querySelector('#video-player');
 
-    constructor(stream: string) {
-        /**
-         * The IVS player can only be used in browsers which support WebAssembly.
-         * You should use `isPlayerSupported` before calling `create`.
-         * Otherwise, wrap `create` in a `try/catch` block, because an error will be thrown in browsers without WebAssembly support.
-         */
-        if (!isPlayerSupported) {
-            throw new Error('IVS Player is not supported in this browser');
-        }
-
-        /**
-         * Web Workers and WASM Workers need to be created via URL. Webpack has created the relative URL for us via file-loader,
-         * now we just have to create the absolute (fully qualified) URL.
-         */
-        const createAbsolutePath = (assetPath: string) => new URL(assetPath, document.URL).toString();
-        const player = this.player = create({
-            wasmWorker: createAbsolutePath(wasmWorkerPath),
-            wasmBinary: createAbsolutePath(wasmBinaryPath),
-        });
-
-        player.attachHTMLVideoElement(this.videoElement);
-        this.attachListeners();
-
-        const versionString: HTMLElement = document.querySelector('.version');
-        versionString.innerText = `Amazon IVS Player version ${player.getVersion()}`;
-
-        this.loadAndPlay(stream);
-
-        // We're adding the demo here so you can play with it in the console.
-        // @ts-ignore
-        window.player = player;
+  constructor(stream: string) {
+    /**
+     * The IVS player can only be used in browsers which support WebAssembly.
+     * You should use `isPlayerSupported` before calling `create`.
+     * Otherwise, wrap `create` in a `try/catch` block, because an error will be thrown in browsers without WebAssembly support.
+     */
+    if (!isPlayerSupported) {
+      throw new Error('IVS Player is not supported in this browser');
     }
 
-    loadAndPlay(stream: string) {
-        const { player } = this;
-        /**
-         * With setAutoplay, we don't need to call play() here to try and start the stream. One of three things will happen:
-         * - Autoplay with sound
-         * - Autoplay muted
-         * - Playback blocked
-         * If autoplay is muted or blocked, the viewer will need to manually interact with the video player in order to unmute or start playback.
-         * See https://developers.google.com/web/updates/2017/09/autoplay-policy-changes for more info on autoplaying video and best practices.
-         * */
-        player.setAutoplay(true);
-        player.load(stream);
+    /**
+     * Web Workers and WASM Workers need to be created via URL. Webpack has created the relative URL for us via file-loader,
+     * now we just have to create the absolute (fully qualified) URL.
+     */
+    const createAbsolutePath = (assetPath: string) =>
+      new URL(assetPath, document.URL).toString();
+    const player = (this.player = create({
+      wasmWorker: createAbsolutePath(wasmWorkerPath),
+      wasmBinary: createAbsolutePath(wasmBinaryPath),
+    }));
+
+    player.attachHTMLVideoElement(this.videoElement);
+    this.attachListeners();
+
+    const versionString: HTMLElement = document.querySelector('.version');
+    versionString.innerText = `Amazon IVS Player version ${player.getVersion()}`;
+
+    this.loadAndPlay(stream);
+
+    // We're adding the demo here so you can play with it in the console.
+    // @ts-ignore
+    window.player = player;
+  }
+
+  loadAndPlay(stream: string) {
+    const { player } = this;
+    /**
+     * With setAutoplay, we don't need to call play() here to try and start the stream. One of three things will happen:
+     * - Autoplay with sound
+     * - Autoplay muted
+     * - Playback blocked
+     * If autoplay is muted or blocked, the viewer will need to manually interact with the video player in order to unmute or start playback.
+     * See https://developers.google.com/web/updates/2017/09/autoplay-policy-changes for more info on autoplaying video and best practices.
+     * */
+    player.setAutoplay(true);
+    player.load(stream);
+  }
+
+  private attachListeners() {
+    const { player } = this;
+    for (let state of Object.values(PlayerState)) {
+      player.addEventListener(state, () => {
+        console.log(state);
+      });
     }
 
-    private attachListeners() {
-        const { player } = this;
-        for (let state of Object.values(PlayerState)) {
-            player.addEventListener(state, () => {
-                console.log(state);
-            });
-        }
+    player.addEventListener(PlayerEventType.INITIALIZED, () => {
+      console.log('INITIALIZED');
+    });
 
-        player.addEventListener(PlayerEventType.INITIALIZED, () => {
-            console.log('INITIALIZED');
-        });
+    player.addEventListener(PlayerEventType.ERROR, (error: PlayerError) => {
+      const statusTooManyRequests = 429;
+      if (
+        error.type === ErrorType.NOT_AVAILABLE &&
+        error.code === statusTooManyRequests
+      ) {
+        console.error('Concurrent-viewer limit reached', error);
+      } else {
+        console.error('ERROR', error);
+      }
+    });
 
-        player.addEventListener(PlayerEventType.ERROR, (error: PlayerError) => {
-            const statusTooManyRequests = 429;
-            if (error.type === ErrorType.NOT_AVAILABLE && error.code === statusTooManyRequests) {
-                console.error('Concurrent-viewer limit reached', error);
-            } else {
-                console.error('ERROR', error);
-            }
-        });
+    player.addEventListener(
+      PlayerEventType.QUALITY_CHANGED,
+      (quality: Quality) => {
+        console.log('QUALITY_CHANGED', quality);
+      }
+    );
 
-        player.addEventListener(PlayerEventType.QUALITY_CHANGED, (quality: Quality) => {
-            console.log('QUALITY_CHANGED', quality);
-        });
+    // This event fires when text cues are encountered, such as captions or subtitles
+    player.addEventListener(PlayerEventType.TEXT_CUE, (cue: TextCue) => {
+      console.log('TEXT_CUE', cue.startTime, cue.text);
+    });
 
-        // This event fires when text cues are encountered, such as captions or subtitles
-        player.addEventListener(PlayerEventType.TEXT_CUE, (cue: TextCue) => {
-            console.log('TEXT_CUE', cue.startTime, cue.text);
-        });
-
-        // This event fires when embedded Timed Metadata is encountered
-        player.addEventListener(PlayerEventType.TEXT_METADATA_CUE, (cue: TextMetadataCue) => {
-            console.log('Timed metadata', cue.text);
-        });
-    }
+    // This event fires when embedded Timed Metadata is encountered
+    player.addEventListener(
+      PlayerEventType.TEXT_METADATA_CUE,
+      (cue: TextMetadataCue) => {
+        console.log('Timed metadata', cue.text);
+      }
+    );
+  }
 }
 
 const demo = new PlayerDemo(getFormStream());
--- webpack.config.js
+++ webpack.config.js
@@ -1,89 +1,81 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
-
 /**
  * We use webpack in this project to demonstrate how to build the SDK.
  * Other packagers should work too, but have not been tested.
  * This config also transpiles our demo code TypeScript code to ES5.
  */
 module.exports = {
-    mode: 'development',
-    devtool: 'cheap-source-map',
-    entry: {
-        'npm-sdk': path.resolve(__dirname, 'samples/npm-sdk/npm-sdk.ts'),
-        'cloud-player':path.resolve(__dirname, 'samples/cloud-player/cloud-player.ts'),
-        'videojs': path.resolve(__dirname, 'samples/videojs/videojs.ts')
+  mode: 'development',
+  devtool: 'cheap-source-map',
+  entry: {
+    'npm-sdk': path.resolve(__dirname, 'samples/npm-sdk/npm-sdk.ts'),
+    'cloud-player': path.resolve(
+      __dirname,
+      'samples/cloud-player/cloud-player.ts'
+    ),
+    videojs: path.resolve(__dirname, 'samples/videojs/videojs.ts'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].js',
+  },
+  devServer: {
+    compress: false,
+    static: [path.resolve(__dirname, 'dist')],
+    host: '0.0.0.0',
+    allowedHosts: ['.repl.it', '.repl.co', '.repl.run'],
+    devMiddleware: {
+      index: 'index.html',
     },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    output: {
-        path: __dirname +  '/dist',
-        filename: '[name].js'
-    },
-    devServer: {
-        disableHostCheck: true,
-        compress: false,
-        contentBase: [
-            path.resolve(__dirname, 'dist')
-        ],
-        index: 'index.html',
-        host: "0.0.0.0",
-        allowedHosts: [
-            ".repl.it",
-            ".repl.co",
-            ".repl.run"
-        ]
-    },
-    plugins: [
-        // Copy our HTML pages to dist/. This project is compiled to expect assets in the same directory.
-        new CopyPlugin({
-            patterns: [
-                { from: 'samples/**/*.html', to: '', flatten: true },
+  },
+  plugins: [
+    // Copy our HTML pages to dist/. This project is compiled to expect assets in the same directory.
+    new CopyPlugin({
+      patterns: [{ from: 'samples/**/*.html', to: '', flatten: true }],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        // This loader compiles the local demo files and not the IVS assets themselves.
+        test: /\.ts$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                modules: 'auto',
+              },
             ],
-        }),
+            ['@babel/preset-typescript'],
+          ],
+          plugins: ['@babel/plugin-proposal-class-properties'],
+        },
+      },
+      {
+        /**
+         * Developers packaging the IVS player into an app are required to resolve and import the following assets via URL:
+         *
+         * 'amazon-ivs-player/dist/assets/amazon-ivs-wasmworker.min.wasm'
+         * 'amazon-ivs-player/dist/assets/amazon-ivs-wasmworker.min.js';
+         *
+         * These assets must not be re-compiled during packaging. Your build tool must import these files as-is, untranspiled.
+         * The webpack file-loader (https://webpack.js.org/loaders/file-loader/) accomplishes this.
+         * Rollup's plugin-url (https://github.com/rollup/plugins/tree/master/packages/url) also seems to do this, but has not been tested.
+         */
+        test: /[\/\\]amazon-ivs-player[\/\\].*dist[\/\\]assets[\/\\]/,
+        loader: 'file-loader',
+        type: 'javascript/auto',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
     ],
-    module: {
-        rules: [
-            {
-                // This loader compiles the local demo files and not the IVS assets themselves.
-                test: /\.ts$/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        ['@babel/preset-env', {
-                            loose: true,
-                            modules: 'auto',
-                        }],
-                        [
-                            '@babel/preset-typescript',
-                        ],
-                    ],
-                    plugins: [
-                        "@babel/plugin-proposal-class-properties",
-                    ]
-                }
-            },
-            {
-                /**
-                 * Developers packaging the IVS player into an app are required to resolve and import the following assets via URL:
-                 *
-                 * 'amazon-ivs-player/dist/assets/amazon-ivs-wasmworker.min.wasm'
-                 * 'amazon-ivs-player/dist/assets/amazon-ivs-wasmworker.min.js';
-                 *
-                 * These assets must not be re-compiled during packaging. Your build tool must import these files as-is, untranspiled.
-                 * The webpack file-loader (https://webpack.js.org/loaders/file-loader/) accomplishes this.
-                 * Rollup's plugin-url (https://github.com/rollup/plugins/tree/master/packages/url) also seems to do this, but has not been tested.
-                 */
-                test: /[\/\\]amazon-ivs-player[\/\\].*dist[\/\\]assets[\/\\]/,
-                loader: 'file-loader',
-                type: 'javascript/auto',
-                options: {
-                    name: '[name].[ext]'
-                }
-            },
-        ]
-    },
+  },
 };
-
